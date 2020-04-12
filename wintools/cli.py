ad49a9dfb4d0a5d6fcecf043a48cdd56cf839e3f
yukkun007 <kato.yutaka@gmail.com>
2020-04-12 15:03:52 +0900

    メッセージが表示されない問題を修正

--- wintools/cli.py
+++ wintools/cli.py
@@ -1,9 +1,6 @@
-import logging
-from wintools.core import foo, hello
-
-
 def main():
-    logging.info("各ツールの起動はwintools/*.batで。")
+    print("各ツールの起動はwintools/*.batで。")
+
 
 if __name__ == "__main__":
     main()
