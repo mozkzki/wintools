e02f41de70ecd00833263ef921e0d41d7cefd857
yukkun007 <kato.yutaka@gmail.com>
2020-04-13 09:37:08 +0900

    s3のアップ先指定機能追加

--- wintools/s3up.py
+++ wintools/s3up.py
@@ -8,6 +8,7 @@ from boto3.session import Session
 from botocore.errorfactory import ClientError
 
 S3_BUCKET = "dummy"
+S3_DIR = "dummy"
 AWS_PROFILE = "dummy"
 
 
@@ -20,6 +21,7 @@ def main():
 
     parser.add_argument("path", help="アップロード対象ファイル/フォルダのフルパス")
     parser.add_argument("-b", "--bucket", help="アップロード先のS3バケット名", default=S3_BUCKET)
+    parser.add_argument("-d", "--dir", help="アップロード先のS3パス", default=S3_DIR)
     parser.add_argument("-p", "--profile", help="AWSのプロファイル名", default=AWS_PROFILE)
 
     args = parser.parse_args()
@@ -29,10 +31,10 @@ def main():
     # ログレベルを DEBUG に変更
     logging.basicConfig(level=logging.INFO, format=formatter)
 
-    upload(args.path, args.bucket, args.profile)
+    upload(args.path, args.bucket, args.dir, args.profile)
 
 
-def upload(path: str, s3_bucket: str, aws_profile: str) -> None:
+def upload(path: str, s3_bucket: str, s3_dir: str, aws_profile: str) -> None:
     if not os.path.exists(path):
         logging.error("target not found. path={}".format(path))
         sys.exit(1)
@@ -43,20 +45,20 @@ def upload(path: str, s3_bucket: str, aws_profile: str) -> None:
         shutil.make_archive(path, "zip", root_dir=path)
         path = path + ".zip"
 
-    logging.info("{} -----> s3://{}".format(path, s3_bucket))
+    logging.info("{} -----> s3://{}/{}".format(path, s3_bucket, s3_dir))
     basename = os.path.basename(path)
 
     # アップロード
-    cmd = "aws s3 cp {} s3://{} --profile {}".format(path, s3_bucket, aws_profile)
+    cmd = "aws s3 cp {} s3://{}/{}/ --profile {}".format(path, s3_bucket, s3_dir, aws_profile)
     logging.info(cmd)
     os.system(cmd)
 
     # アップロードに成功したらs3パスをクリップボードへコピー
-    if is_s3_key_exists(aws_profile, s3_bucket, basename):
+    if is_s3_key_exists(aws_profile, s3_bucket, s3_dir + "/" + basename):
         logging.info("----------------------")
         logging.info("SUCCESS")
         logging.info("----------------------")
-        result = "s3://{}/{}".format(s3_bucket, basename)
+        result = "s3://{}/{}/{}".format(s3_bucket, s3_dir, basename)
         logging.info(result + "   ( copied to clipboad. )")
         logging.info("----------------------")
         pyperclip.copy(result)
